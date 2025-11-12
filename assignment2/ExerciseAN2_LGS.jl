ab428cf5e4b007ca54b1e2fab75d91c00bf177f6
dpirkl <david.pirkl@stud.tu-darmstadt.de>
2025-11-13 00:20:56 +0100

    add: jacobi method

--- assignment2/ExerciseAN2_LGS.jl
+++ assignment2/ExerciseAN2_LGS.jl
@@ -3,12 +3,36 @@ using Printf
 
 
 # ------------------ add solver definitions --------------------------
-function jacobi(A::Matrix, b::Vector, maxiter = 100, epsilon = 1e-8)
-    x = zeros(length(b))                  # example vector init
-    @printf "first elem in x %f" x[1]     # example std out ( index starting from 1(!) )
+function jacobi(A::Matrix, b::Vector, maxiter=100, epsilon=1e-8)
+    x = zeros(length(b))
+    x_new = copy(x)
+    residuals = Float64[]
 
-    # ...
+    # Compute D and R matrices once (independent of x)
+    D = diagm(0 => diag(A))
+    R = A - D
+    Dinv = Diagonal(1.0 ./ diag(A))
 
+    for iter = 1:maxiter
+        x_new = Dinv * (b - R * x)
+
+        # residuals
+        r = b - A * x_new
+        push!(residuals, norm(r))
+
+        if norm(x_new - x, Inf) < epsilon
+            return x_new, residuals
+        end
+
+        # Alternatively, check residual norm
+        # if norm(r) < epsilon
+        #     return x_new, residuals
+        # end
+
+        x = x_new
+    end
+
+    return x_new, residuals
 end
 
 function solveGroundTruth( A::Matrix, b::Vector )
